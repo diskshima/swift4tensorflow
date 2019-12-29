4fe309bffa803dcc855fb28b2af40ce965775147
Daisuke Shimamoto <diskshima@gmail.com>
2019-12-29 13:22:32 +0900

    Download & add TutorialDatasetCSVAPI.swift to repo

--- tutorial/tutorial/main.swift
+++ tutorial/tutorial/main.swift
@@ -17,12 +17,12 @@ func download(from sourceString: String, to destinationString: String) {
     let destination = URL(fileURLWithPath: destinationString)
     let data = try! Data.init(contentsOf: source)
     try! data.write(to: destination)
-    print("Downloaded \(destinationString) to \(FileManager.default.currentDirectoryPath)")
+    print("Downloaded \(destination.lastPathComponent) to \(FileManager.default.currentDirectoryPath)")
 }
 
 download(
     from: "https://raw.githubusercontent.com/tensorflow/swift/master/docs/site/tutorials/TutorialDatasetCSVAPI.swift",
-    to: "TutorialDatasetCSVAPI.swift")
+    to: "tutorial/tutorial/TutorialDatasetCSVAPI.swift")
 
 let trainDataFilename = "iris_training.csv"
 download(from: "http://download.tensorflow.org/data/iris_training.csv", to: trainDataFilename)
